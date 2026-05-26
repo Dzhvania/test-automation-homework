eee071bafe5deef91c67e03c2454506b255b13a2
Dimitri Zhvania <dzhvania@credo.ge>
2026-05-27 02:38:38 +0400

    [Dimitri Z.] Add Homework3.java with Dimitri Zhvania

---
+++ src/main/java/Homework3.java
@@ -0,0 +1,10 @@
+/** Homework 3 -Github PR domenstration
+ * @author Dimitri Zhvania
+ */
+public class Homework3 {
+    public static void main(String[] args) {
+        String author = "Dimitri Zhvania";
+        int currentYear = 2026;
+        System.out.println("[" + author + "] First Pull Request - " + currentYear );
+    }
+}
